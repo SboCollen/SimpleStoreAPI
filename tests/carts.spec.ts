b03103d61fd96a0820cfbee8032c63725d0ab5ec
Sibonelo67 <collensbonelo67@gmail.com>
2026-06-19 23:37:22 +0200

    carts imports

---
+++ tests/carts.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+import {validatedBaseUrl} from  '../config/api.env.js';
+
+//TODO: I need to add more tests for the carts endpoints for example, I need to test the POST /carts endpoint and the DELETE /carts/:id endpoint
+
