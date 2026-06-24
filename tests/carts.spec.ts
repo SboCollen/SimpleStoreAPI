e4b62833d46a3244cea369c5f99c4b1594efd6eb
Sibonelo67 <collensbonelo67@gmail.com>
2026-06-24 22:18:48 +0200

    created more test cases under carts

--- tests/carts.spec.ts
+++ tests/carts.spec.ts
@@ -4,11 +4,12 @@ import { error } from 'console';
 
 //TODO: I need to add more tests for the carts endpoints for example, I need to test the POST /carts endpoint and the DELETE /carts/:id endpoint
 
-test.describe('Store Carts', () => {
+test.describe('Store Carts.', () => {
 
-    test('should create carts', async ({request}) => {
+    test('should create carts.', async ({request}) => {
         const response = await request.post(`${validatedBaseUrl}/carts`);
         expect(response.status()).toBe(201);
+
         const cart = await response.json();
         expect(cart.created).toBe(true); 
         expect(cart.created).toBeDefined();
@@ -17,9 +18,15 @@ test.describe('Store Carts', () => {
     });
 
 
+    test('should get all the cats.', async ({request}) => {
+        const response = await request.get(`${validatedBaseUrl}/carts`);
+        expect(response.status()).toBe(200);
 
-    test('should have carts', async ({ request }) => {
+        const carts = await response.json();
+        expect(carts.length).toBeGreaterThan(0);
+    });
 
+    test('should get a cart by it id.', async ({ request }) => {
         const response = await request.get(`${validatedBaseUrl}/carts/o43PqgkYGuKZg8_dPa8EY`);
         expect(response.status()).toBe(200);
       
@@ -27,16 +34,50 @@ test.describe('Store Carts', () => {
         expect(cart.items).toBeDefined();
         expect(cart.created).toBeDefined();
        
-      });
+    });
+    
+    test('should add products on the specific cart.', async({request}) => {
+
+        const productToAdd = {
+                "productId": 1225, //use new product id
+                "quantity": 4
+            };
+
+        const response = await request.post(`${validatedBaseUrl}/carts/o43PqgkYGuKZg8_dPa8EY/items`, {
+            data: productToAdd        
+        });
+
+        expect(response.status()).toBe(201);
+
+    });
+
+    test('should add a product that is already on the cart. should get an error.', async({request}) => {
+        
+        const productToAdd = {
+                "productId": 1225, //use new product id
+                "quantity": 4
+            };
+
+        const response = await request.post(`${validatedBaseUrl}/carts/o43PqgkYGuKZg8_dPa8EY/items`, {
+            data: productToAdd 
+        });
+
+        expect(response.status()).toBe(400);
+        const products = await response.json();
+        expect(products).toHaveProperty('error');
+    });
+
+    test('should get items in a cart.', async ({ request }) => {
+        const response = await request.get(`${validatedBaseUrl}/carts/o43PqgkYGuKZg8_dPa8EY/items`);
+        expect(response.status()).toBe(200);
+
+        const carts = await response.json();
+        expect(carts.length).toBeGreaterThan(0);
+
+    });
+    
     
 
 
-
-
-    }
-
-
-
-    
-);
+});
 
